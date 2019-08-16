d7efd645708a8b932a3e477bf6c09786956051e5
knut-sdpark <sdp1422@naver.com>
2019-08-16 23:19:48 +0900

    created 'ProfileActivity' Activity

---
+++ AuthCodeApp/app/src/main/java/com/example/sangdonpark/authcodeapp/presenter/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.sangdonpark.authcodeapp.presenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sangdonpark.authcodeapp.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
