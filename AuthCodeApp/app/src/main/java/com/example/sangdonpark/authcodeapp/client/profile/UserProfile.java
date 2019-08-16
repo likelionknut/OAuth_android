17913e83c830f5e091e74363b307120a9be492bb
knut-sdpark <sdp1422@naver.com>
2019-08-16 22:52:21 +0900

    created 'UserProfile.java'

---
+++ AuthCodeApp/app/src/main/java/com/example/sangdonpark/authcodeapp/client/profile/UserProfile.java
@@ -0,0 +1,15 @@
+package com.example.sangdonpark.authcodeapp.client.profile;
+
+public class UserProfile {
+
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
